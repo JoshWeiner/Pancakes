5ba226676d597eb8d876e5eed68b884d1c8fb156
Alvin Ye <aye4@stuy.edu>
2018-04-11 22:29:30 -0400

    finished ALStack

--- ALStack.java
+++ ALStack.java
@@ -14,11 +14,24 @@ public class ALStack<T> implements Stack<T>{
     }
 
     public T pop(){
-    	return list.remove(list.size() - 1);
+        if(isEmpty()){
+            return null;
+        }
+        else{
+    	   return list.remove(list.size() - 1);
+        }
     }
 
     public void push( T x){
     	list.add(x);
+    }
+
+    public String toString(){
+        String retVal = "[";
+        for(int i = 0; i < list.size(); i++){
+            retVal = retVal + list.get(i) + ",";
+        }
+        return retVal.substring(0, retVal.length() - 1) + "]";
     }
 
 
--- Stckr.java
+++ Stckr.java
@@ -7,8 +7,20 @@ public class Stckr
 {
     public static void main( String[] args )
     {
+
+    //only uncomment one of these at a time...
 	Stack<String> cakes = new ALStack<String>();
 	//Stack<Z> cakes = new LLStack<Z>();
+
+	System.out.println(cakes.pop()); //tests to see if pop() works on empty list
+	cakes.push("a");
+	cakes.push("b");
+	cakes.push("c");
+	System.out.println(cakes);
+
+	cakes.pop();
+	System.out.println(cakes);
+
 
 	//...
 	
